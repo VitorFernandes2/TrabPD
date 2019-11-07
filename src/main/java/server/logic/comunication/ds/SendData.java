bad40b254e10f9a1445f47576818286da79da5b2
VitorFernandes2 <vfherprog@gmail.com>
2019-11-07 21:32:07 +0000

    Create SendData.java

---
+++ src/main/java/server/logic/comunication/ds/SendData.java
@@ -0,0 +1,16 @@
+package server.logic.comunication.ds;
+
+public class SendData extends Thread{
+
+    public SendData() {
+
+        
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+}
