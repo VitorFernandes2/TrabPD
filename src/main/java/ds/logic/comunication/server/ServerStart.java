e74927681e9b7d8a4dca31c87c2dd0156d61c38b
VitorFernandes2 <vfherprog@gmail.com>
2019-11-07 12:44:56 +0000

    Update ServerStart.java

--- src/main/java/ds/logic/comunication/server/ServerStart.java
+++ src/main/java/ds/logic/comunication/server/ServerStart.java
@@ -1,15 +1,34 @@
 package ds.logic.comunication.server;
+
 import org.json.simple.JSONObject;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.HashMap;
 
 public class ServerStart extends Thread {
 
     private static int NumberOfThreads = 0;
     private int ThreadNumber;
+
+    //Objecto JSON para receber informação do servidor
     private JSONObject ReceiveJsonObj;
 
-    public ServerStart() {
+    //Dados do DS
+    private String IP;
+    private String Port;
+
+    //Socket de controlo dos Servidores
+    DatagramSocket Socket;
+    DatagramPacket Packet;
+
+    //Mapa de IP'S/Portos dos Servidores
+    HashMap<String, String> ServerMaps;
+
+    public ServerStart(String IP, String Port) {
         ReceiveJsonObj = new JSONObject();
         ThreadNumber = ++NumberOfThreads;
+        this.IP = IP;
+        this.Port = Port;
     }
 
     public static int getNumberOfThreads() {
