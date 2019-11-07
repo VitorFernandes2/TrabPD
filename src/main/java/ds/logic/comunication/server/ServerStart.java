c579419b209d06c0b6422451c303516bc3fe1af3
VitorFernandes2 <vfherprog@gmail.com>
2019-11-07 21:26:01 +0000

    Thread de controlo dos Servers, quase feita

--- src/main/java/ds/logic/comunication/server/ServerStart.java
+++ src/main/java/ds/logic/comunication/server/ServerStart.java
@@ -1,52 +1,82 @@
 package ds.logic.comunication.server;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.HashMap;
 
 public class ServerStart extends Thread {
 
+    private final int TotalBytes = 4096;
+
     private static int NumberOfThreads = 0;
     private int ThreadNumber;
-
-    //Objecto JSON para receber informação do servidor
-    private JSONObject ReceiveJsonObj;
+    private boolean finish;
 
     //Dados do DS
     private String IP;
     private String Port;
 
-    //Socket de controlo dos Servidores
-    DatagramSocket Socket;
-    DatagramPacket Packet;
-
     //Mapa de IP'S/Portos dos Servidores
-    HashMap<String, String> ServerMaps;
+    private HashMap<String, String> ServerMaps;
 
-    public ServerStart(String IP, String Port) {
-        ReceiveJsonObj = new JSONObject();
-        ThreadNumber = ++NumberOfThreads;
+    public ServerStart(String IP, String Port, HashMap<String, String> ServerMaps)
+    {
+
+        this.ThreadNumber = ++NumberOfThreads;
+        this.finish = false;
         this.IP = IP;
         this.Port = Port;
-    }
+        this.ServerMaps = ServerMaps;
 
-    public static int getNumberOfThreads() {
-        return NumberOfThreads;
     }
 
     public int getThreadNumber() {
         return ThreadNumber;
     }
 
-    @Override
-    public synchronized void start() {
-        super.start();
-    }
-
+    //Thread para receber todos os novos servidores
     @Override
     public void run() {
-        super.run();
+
+        DatagramSocket Socket;
+        DatagramPacket Packet;
+
+        try {
+
+            Socket = new DatagramSocket(Integer.parseInt(Port));
+
+            while (!finish) {
+
+                byte[] buf = new byte[TotalBytes];
+                Packet = new DatagramPacket(buf, buf.length);
+                Socket.receive(Packet);
+
+                String SObj = new String(Packet.getData(), 0, Packet.getLength());
+                JSONParser JsonParser = new JSONParser();
+                JSONObject JObj = (JSONObject) JsonParser.parse(SObj);
+
+                System.out.println(JObj);
+
+                Socket.close();
+
+                finish = true;
+
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
+
 
 }
