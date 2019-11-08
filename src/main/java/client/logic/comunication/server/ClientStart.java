3882a7acd5b711053ffcb912c14a82355d0dcfad
Luisilva99 <44380629+Luisilva99@users.noreply.github.com>
2019-11-08 12:11:45 +0000

    Desenvolvimento do ClientStart
    Comecei por receber o IP e Porto do DS ao qual eu enviarei o meu pedido de IP e Porto para ligação ao Servidor e o tratamento dos objetos JSONObject, assim como um esqueleto de função para atribuição do JSONObject a enviar ao Servidor como pedido, ainda não fiz o envio do pedido para o Servidor e a sua resposta.

--- src/main/java/client/logic/comunication/server/ClientStart.java
+++ src/main/java/client/logic/comunication/server/ClientStart.java
@@ -1,8 +1,13 @@
 package client.logic.comunication.server;
 
+import java.io.IOException;
 import java.net.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class ClientStart extends Thread {
@@ -10,6 +15,10 @@ public class ClientStart extends Thread {
     //Objetos para os comandos do utilizador
     private Scanner Sc;
     private String [] Command;
+    private String Com;
+    
+    //Objeto do nome do utilizador / posição provisória até confirmação por parte do grupo
+    private String username;
     
     //Objetos para a ligação entre aplicações
     private String Port;
@@ -21,40 +30,104 @@ public class ClientStart extends Thread {
     private DatagramPacket DPacketReceive;
     
     //Objetos para a ligação TCP com o Servidor
-    private ServerSocket ServerSocket;
     private Socket Socket;
     
     //Objecto JSON para receber informaÃ§Ã£o do servidor
+    private static int TotalBytes = 4096;
     private JSONObject ReceiveJsonObj;
+    private JSONObject SendJsonObj;
 
     public ClientStart(String Port, String IP) {
         this.Port = Port;
         this.IP = IP;
         this.Sc = new Scanner(System.in);
         this.ReceiveJsonObj = new JSONObject();
+        this.SendJsonObj = new JSONObject();
+    }
+
+    //Funções a usar caso o Cliente tenha de ser relocado para um novo servidor
+    public void setPort(String Port) {
+        this.Port = Port;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
     }
     
     @Override
     public void run() {
-        //Código de ligação:
         
-        //******************
-        
-        while(true){
-            //Implementação de código de envio de pedidos de comando
+        try {
+            //Código de ligação inicial com o DS:
             
-            //******************************************************
-            System.out.printf("> ");
-            String Com = Sc.nextLine();
+            DSocket = new DatagramSocket(Integer.parseInt(Port));
             
-            //Fazer o corte dos comandos
+            SendJsonObj.put("IP", String.valueOf(InetAddress.getLocalHost()));
+            SendJsonObj.put("Port", String.valueOf(DSocket.getLocalPort()));
             
-            //**************************
+            String sendDataJSON = SendJsonObj.toString();
             
-            if(Com.equalsIgnoreCase("exit")){
-                break;
+            byte [] send = sendDataJSON.getBytes();
+            
+            DPacketSend = new DatagramPacket(send, send.length, InetAddress.getByName(IP), Integer.parseInt(Port));
+            
+            DSocket.send(DPacketSend);
+            
+            byte [] receive = new byte[TotalBytes];
+            
+            DPacketReceive = new DatagramPacket(receive, receive.length);
+            
+            String SObj = new String(DPacketReceive.getData(), 0, DPacketReceive.getLength());
+            JSONParser JsonParser = new JSONParser();
+            ReceiveJsonObj = (JSONObject) JsonParser.parse(SObj);
+
+            System.out.println(ReceiveJsonObj);
+            
+            //Passagem dos valores a usar para TCP:
+            IP = (String) ReceiveJsonObj.get("IP");
+            Port = (String) ReceiveJsonObj.get("Port");
+            
+            DSocket.close();
+            
+            //***********************************
+            
+            //Inicio da ligação TCP com o Servidor
+            
+            Socket = new Socket(IP, Integer.parseInt(Port));
+            
+            while(Com.equalsIgnoreCase("exit") || Com.equalsIgnoreCase("logout")){
+                //Implementação de código de envio de pedidos de comando
+                
+                System.out.printf("> ");
+                Com = Sc.nextLine();
+                
+                Command = Com.trim().split(";");
+                
+                //Se o comando for apenas uma palavra
+                if(Command.length < 2){
+                    //Código do tratamento de saida do utilizador com o envio do fim de sessão para o servidor
+                    
+                    //****************************************************************************************
+                    continue;
+                }
+                
+                SendJsonObj = tratmentOfCommands(Command);
+                
+                //Local de inicío de envio e resposta do Cliente-Servidor
+                
+                //******************************************************
             }
             
+            Socket.close();
+            
+            //************************************
+            
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         
     }
@@ -62,6 +135,30 @@ public class ClientStart extends Thread {
     @Override
     public synchronized void start() {
         super.start();
+    }
+
+    private JSONObject tratmentOfCommands(String[] Command) {
+        
+        JSONObject AuxJsonObj = new JSONObject();
+        
+        for(String comando : Command){
+            
+            String [] div = comando.split("|");
+            
+            if(div[0].equalsIgnoreCase("Tipo")){
+                
+                if(div[1].equalsIgnoreCase("login")){
+                    AuxJsonObj.put("Tipo", "login");
+                }
+                else if(div[1].equalsIgnoreCase("Logout")){
+                    AuxJsonObj.put("Tipo", "logout");
+                }
+            }
+            //else if(div[0])
+            //Falta a continuação da realização desta função!
+        }
+        
+        return AuxJsonObj;
     }
     
 }
