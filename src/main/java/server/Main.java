aa10b8b3a565b8bb13dc7e89d7bbf3764372e258
VitorFernandes2 <vhferprog@gmail.com>
2020-01-03 01:36:29 +0000

    Ligeiro update

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,6 +1,5 @@
 package server;
 
-import Rest.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import server.comunicationInterface.ComunicationInterface;
